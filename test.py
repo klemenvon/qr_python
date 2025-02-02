772a9b8758df4a67e7f96287902720b9304746d8
Klemen Voncina <klemenvon@gmail.com>
2025-02-02 17:20:42 +0100

    Change argument style. Underscore makes no sense.

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ parser = ArgumentParser()
 parser.add_argument('-v', '--version', type=int, default=1, help='QR Code version')
 parser.add_argument('-m', '--module_size', type=int, default=5, help='Module size')
 parser.add_argument('-s', '--save', type=str, help='Filename to save QR Code')
-parser.add_argument('--show_mask', action='store_true', help='Show only data area.')
+parser.add_argument('--show-mask', action='store_true', help='Show only data area.')
 args = parser.parse_args()
 
 data = 'Hello World! This is just here for padding. Hi.'
